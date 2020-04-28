754d8f48028e94f405bf092696af3d61c6d5185e
Lucas Rutkoski <lucas.rtk@hotmail.com>
2020-04-28 13:21:47 -0300

    Commit inicial do projeto
    O commit inicial possui os modelos de grupo e usuário e um controller com endpoints para cadastrar grupos, obter dados de um grupo e gerar a chave de ingresso de um grupo. No leia-me, á um mini passo-a-passo com as etapas necessárias para poder compilar e trabalhar.

---
+++ eeduca-api/Controllers/GruposController.cs
@@ -0,0 +1,103 @@
+﻿using eeduca_api.Models;
+using eeduca_api.Database;
+using System;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Linq;
+
+namespace eeduca_api.Controllers
+{
+    public class GruposController : ApiController
+    {
+        //!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+        //CÓDIGO TEMPORÁRIO!!!! REMOVER NO FUTURO
+        //!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+        private Usuario ObterUsuarioDeTestes()
+        {
+            Usuario usuario = MySQLContext.ObterInstancia.Usuarios
+                                                         .Where(u => u.Email.Equals("lucas.rtk@hotmail.com"))
+                                                         .FirstOrDefault();
+
+            if (usuario == null) 
+            {
+                MySQLContext.ObterInstancia.Usuarios.Add(new Models.Usuario
+                {
+                    Email = "lucas.rtk@hotmail.com",
+                    Nome = "Lucas Rutkoski",
+                    Senha = Convert.FromBase64String("273a0bfec91744347e874922ad05f4380e401937508ebb28") //Senha: 1
+                });
+                MySQLContext.ObterInstancia.SaveChanges();
+            }
+
+            return usuario;
+        }
+
+        [Route("api/Grupos/CriarGrupo")]
+        [HttpPost]
+        public HttpResponseMessage CriarGrupo(string NomeGrupo)
+        {
+            HttpResponseMessage retorno = new HttpResponseMessage();
+            Grupo NovoGrupo;
+
+            if (String.IsNullOrWhiteSpace(NomeGrupo))
+            {
+                retorno.ReasonPhrase = "O nome do grupo não pode estar em branco!";
+                retorno.StatusCode = HttpStatusCode.BadRequest;
+                return retorno;
+            }
+
+            try
+            {
+                NovoGrupo = new Grupo
+                {
+                    Nome = NomeGrupo,
+                    Administrador = ObterUsuarioDeTestes()
+                };
+
+                MySQLContext.ObterInstancia.Grupos.Add(NovoGrupo);
+                MySQLContext.ObterInstancia.SaveChanges();
+            }
+            catch (Exception e)
+            {
+                while (e.InnerException != null) e = e.InnerException;
+
+                retorno.Content = new StringContent(e.Message + "\n" + e.StackTrace);
+                retorno.StatusCode = HttpStatusCode.InternalServerError;
+                return retorno;
+            }
+
+            retorno.ReasonPhrase = "Grupo criado com sucesso!";
+            retorno.StatusCode = HttpStatusCode.OK;
+            retorno.Headers.Add("GrupoId", NovoGrupo.Id.ToString());
+            return retorno;
+        }
+
+        [Route("api/Grupos/ObterGrupo")]
+        [HttpGet]
+        public Grupo ObterGrupo(int Id)
+        {
+            return MySQLContext.ObterInstancia.Grupos
+                                              .Where(g => g.Id.Equals(Id))
+                                              .FirstOrDefault();
+        }
+
+        [Route("api/Grupos/ObterChaveIngresso")]
+        [HttpGet]
+        public string ObterChaveIngresso(int Id)
+        {
+            Grupo grupo = ObterGrupo(Id);
+
+            if (grupo == null)
+                return null;
+
+            if (String.IsNullOrWhiteSpace(grupo.Chave))
+            {
+                grupo.GerarChaveIngresso();
+                MySQLContext.ObterInstancia.SaveChanges();
+            }
+
+            return grupo.Chave;
+        }
+    }
+}
---
+++ eeduca-api/Database/MySQLContext.cs
@@ -0,0 +1,38 @@
+﻿using eeduca_api.Models;
+using MySql.Data.Entity;
+using System.Data.Entity;
+
+namespace eeduca_api.Database
+{
+    [DbConfigurationType(typeof(MySqlEFConfiguration))]
+    public class MySQLContext: DbContext
+    {
+        private static MySQLContext instancia = null;
+        private static readonly object Trava = new object();
+
+        public static MySQLContext ObterInstancia
+        {
+            get
+            {
+                if (instancia == null)
+                {
+                    lock(Trava)
+                    {
+                        if (instancia == null)
+                        {
+                            instancia = new MySQLContext();
+                        }
+                    }                    
+                }
+                return instancia;
+            }
+        }
+
+        public DbSet<Usuario> Usuarios { get; set; }
+        public DbSet<Grupo> Grupos { get; set; }
+
+        public MySQLContext(): base("name=ConexaoMySQL")
+        {
+        }
+    }
+}
